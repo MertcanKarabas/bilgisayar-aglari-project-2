dfe05187c00ceb5482073c5b2066e266acdbdd3d
Mertcan Karabaş <64467624+MertcanKarabas@users.noreply.github.com>
2023-06-04 02:16:41 +0300

    Design
    Genel olarak design üstünde çalıştım ve design'ı bitirdim sayılır.

---
+++ MesajlasmaApp/src/main/java/Server/EmojiEx.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Server;
+
+/**
+ *
+ * @author mertcankarabas
+ */
+public class EmojiEx {
+    
+    private static final String ROCKET = "\uD83D\uDE80";
+    public static void main(String[] args) {
+        System.out.println(ROCKET + "Ready");
+    }
+}
